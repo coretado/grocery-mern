4b8fa7426dd36a38fa85e785a04890c86b13d309
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-03-14 10:12:48 -0700

    Added tokens to add and delete functionality.

--- client/src/store/actions/itemActions.js
+++ client/src/store/actions/itemActions.js
@@ -1,38 +1,55 @@
-import axios from 'axios';
-import { ADD_ITEM, DELETE_ITEM, GET_ITEMS, LOAD_ITEMS_ERROR, LOADING_ITEMS, } from './types';
+import axios from "axios";
+import {
+  ADD_ITEM,
+  DELETE_ITEM,
+  GET_ITEMS,
+  LOAD_ITEMS_ERROR,
+  LOADING_ITEMS
+} from "./types";
+import { tokenConfig } from "./authActions";
+import { returnErrors } from "./errorActions";
 
-export const addItem = (item) => (dispatch) => {
+export const addItem = item => (dispatch, getState) => {
   axios
-  .post('/api/items', item)
-  .then(res =>
-    dispatch({
-      type: ADD_ITEM, 
-      item: res.data
-    })
-  );
+    .post("/api/items", item, tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: ADD_ITEM,
+        item: res.data
+      })
+    )
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
-export const deleteItem = (id) => dispatch => {
+export const deleteItem = id => (dispatch, getState) => {
   axios
-  .delete(`/api/items/${id}`)
-  .then(res =>
-    dispatch({
-      type: DELETE_ITEM, 
-      id
-    })
-  );
+    .delete(`/api/items/${id}`, tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: DELETE_ITEM,
+        id
+      })
+    )
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
-export const getItems = () => (dispatch) => {
+export const getItems = () => dispatch => {
   dispatch(loadItems());
   axios
-  .get('/api/items')
-  .then(res => 
-    dispatch({
-      type: GET_ITEMS, 
-      items: res.data
-    })
-  );
+    .get("/api/items")
+    .then(res =>
+      dispatch({
+        type: GET_ITEMS,
+        items: res.data
+      })
+    )
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 export const loadItems = () => {
@@ -44,5 +61,5 @@ export const loadItems = () => {
 export const loadItemsError = () => {
   return {
     type: LOAD_ITEMS_ERROR
-  }
-}
+  };
+};
